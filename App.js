5fc0396d4abf28fa75c6c177ef2a015e1d9ac52d
nathanmalone99 <61420616+nathanmalone99@users.noreply.github.com>
2022-04-04 16:17:14 +0100

    Finished

--- App.js
+++ App.js
@@ -1,14 +1,63 @@
+import React, {useState, useEffect} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, FlatList, ActivityIndicator} from 'react-native';
+
 
 export default function App() {
+
+  const menuURL = 'https://my-burger-api.herokuapp.com/burgers/';
+
+  const [isLoading, setLoading] = useState(true); 
+  const [data, setData] = useState();
+  const [name, setName] = useState();
+  const [restaurant, setRestaurant] = useState();
+  const [description, setDescription] = useState();
+
+
+
+  useEffect(() => {
+    fetch(menuURL)
+    .then((response) => response.json())
+    
+    .then((json) => {
+      console.log('first item', json [0])
+      console.log('first name :', json[0].name)
+      setData(json)
+
+      // console.log('data:', data)
+      // console.log('can u see me', json) 
+      // setData(json.data);
+      // setName(json.name);
+      
+      // setRestaurant(json.restaurant);
+      // console.log('restaurant:', json.restaurant)
+      // setDescription(json.description);
+    })
+    .catch((error) => alert(error))
+    .finally(setLoading(false));
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      {isLoading ? (<ActivityIndicator /> 
+      ) : (
+        <View>
+          {/* <Text>{name}</Text> */}
+      <FlatList data={data}
+        keyExtractor={({ id },
+        index) => id} 
+        renderItem={({item}) => {
+        
+          {/* <Text>
+            {item.name}, {item.restaurantnt}, {item.description}
+          </Text> */}
+        }}
+      /> 
+      </View>
+      )}
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
